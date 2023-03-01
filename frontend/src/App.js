3e6428de51f54abf2ccc48dfe343e07b6e984268
jwc20 <choij2015@gmail.com>
2023-03-01 14:20:32 -0800

    Updated react app to fetch from django api using axios.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,46 @@
-import React from "react";
-
 import { useEffect, useState } from "react";
 
-const list = [
-  { id: 1, title: "1", body: "1" },
-  { id: 2, title: "2", body: "2" },
-  { id: 3, title: "3", body: "3" },
-];
+import axios from "axios";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const url = "http://127.0.0.1:8000/api/";
+
+  useEffect(() => {
+    const getAllTdos = async () => {
+      try {
+        const response = await axios.get(url);
+        setIsLoading(false);
+        const allTodos = response.data;
+        console.log(allTodos);
+        setTodos(allTodos);
+      } catch (error) {
+        setIsLoading(false);
+        setIsError(true);
+        console.log(error);
+      }
+    };
+
+    getAllTdos();
+  }, []);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
     <div className="App">
-      {list.map((item, index) => (
-        <div key={item.id}>
-          <h1>{item.title}</h1>
-          <p>{item.body}</p>
-        </div>
-      ))}
+      {todos &&
+        todos.map((item, index) => (
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+            <p>{item.body}</p>
+          </div>
+        ))}
+
+      {isError && <div>Error fetching data.</div>}
     </div>
   );
 }
