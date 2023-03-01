551ecec686dadd7821a90d03f6f9c8ecb8f431f9
jwc20 <choij2015@gmail.com>
2023-03-01 10:58:21 -0800

    Updated to use viewsets and routers.

--- backend/todos/urls.py
+++ backend/todos/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path, include
 
-from .views import ListTodoView, DetailTodoView
+# from .views import ListTodoView, DetailTodoView
+
+from rest_framework.routers import SimpleRouter
+from .views import TodoViewSet
 
 
-urlpatterns = [
-    path("<int:pk>/", DetailTodoView.as_view(), name="detail"),
-    path("", ListTodoView.as_view(), name="list"),
-]
+
+
+# urlpatterns = [
+#     path("<int:pk>/", DetailTodoView.as_view(), name="detail"),
+#     path("", TodoViewSet.as_view(), name="list"),
+# ]
+
+router = SimpleRouter() 
+router.register('', TodoViewSet, basename='todos')
+
+urlpatterns = router.urls
+
+
+
--- backend/todos/views.py
+++ backend/todos/views.py
@@ -1,13 +1,19 @@
-from rest_framework import generics
+from rest_framework import viewsets
 from .models import Todo
 from .serializers import TodoSerializer
 
 
-class ListTodoView(generics.ListAPIView):
-    queryset = Todo.objects.all() 
-    serializer_class = TodoSerializer
+# class ListTodoView(generics.ListAPIView):
+#     queryset = Todo.objects.all() 
+#     serializer_class = TodoSerializer
 
 
-class DetailTodoView(generics.RetrieveAPIView):
+# class DetailTodoView(generics.RetrieveAPIView):
+#     queryset = Todo.objects.all() 
+#     serializer_class = TodoSerializer
+
+
+
+class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all() 
     serializer_class = TodoSerializer
